a63d67617edabfd5024e37daef0b16742f675af6
iyomumx <mo.yi.sam@gmail.com>
2014-05-03 16:06:15 +0800

    版本更新

--- gen_quicksearch/stdafx.cpp
+++ gen_quicksearch/stdafx.cpp
@@ -4,9 +4,9 @@ using namespace System;
 using namespace System::Reflection;
 
 #define PLUGIN_VER 0x11
-#define PLUGIN_NAME "Quick Search In Current Playlist v0.0.3"
+#define PLUGIN_NAME "Quick Search In Current Playlist v0.0.4"
 
-[assembly:AssemblyVersionAttribute("1.0.0.3")];
+[assembly:AssemblyVersionAttribute("1.0.0.4")];
 [assembly:AssemblyCultureAttribute("zh")];
 [assembly:AssemblyTitleAttribute("WinampQuickSearch")];
 [assembly:AssemblyCopyrightAttribute("iyomumx @ 2014")];
